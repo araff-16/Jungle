8e8716c430fc705c5515bd1918f20a505b244cab
araff-16 <alex_raffa16@hotmail.com>
2022-01-07 00:51:37 +0000

    user autherntication complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    puts "DID IT PASS firsttest"
+    if user && user.authenticate(params[:password])
+      puts "DID IT PASS secondtest"
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # If user's login doesn't work, send them back to the login form.
+      puts "DID IT PASS thirdtest"
+      redirect_to :new_session
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to :new_session
   end
 
 end
