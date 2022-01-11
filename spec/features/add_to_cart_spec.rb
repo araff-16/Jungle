4e5618fd6c7e55ff572cfc0ab4847dd10ff99111
araff-16 <alex_raffa16@hotmail.com>
2022-01-11 23:32:36 +0000

    test to see if user can add product to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Users can add a product to the cart" do 
+    #ACT
+    visit root_path
+
+    first('article button.btn-primary').click
+    
+
+    #DEBUG / VERIFY
+    save_screenshot
+
+    expect(page).to have_selector('a', text: 'My Cart (1)')
+  end
+
+
+
+
+end
